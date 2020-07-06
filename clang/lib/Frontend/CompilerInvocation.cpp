0b927db30aa19703c8e529d0d7b869ec3c319ccd
Manoel Roemmer <manoel.roemmer@rwth-aachen.de>
2020-07-06 14:52:32 +0200

    Enable ve-unknown-linux as a omp target

--- clang/lib/Frontend/CompilerInvocation.cpp
+++ clang/lib/Frontend/CompilerInvocation.cpp
@@ -3177,6 +3177,7 @@ static void ParseLangArgs(LangOptions &Opts, ArgList &Args, InputKind IK,
             TT.getArch() == llvm::Triple::nvptx ||
             TT.getArch() == llvm::Triple::nvptx64 ||
             TT.getArch() == llvm::Triple::amdgcn ||
+            TT.getArch() == llvm::Triple::ve ||
             TT.getArch() == llvm::Triple::x86 ||
             TT.getArch() == llvm::Triple::x86_64))
         Diags.Report(diag::err_drv_invalid_omp_target) << A->getValue(i);
